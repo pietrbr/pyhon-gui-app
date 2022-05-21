527536a5ef8b02cc5d8f02be0dfab50e8aff3389
pietrbr <brachdelprever.p@norhteastern.edu>
2022-05-20 20:04:37 -0400

    Finalized implementation fro create and save file
    Put link for the append of rows in csv as TODO

--- GuiApp.py
+++ GuiApp.py
@@ -3,9 +3,8 @@
 # TODO: canopy temeprature si raccoglie attraverso la camera termica
 # TODO: aggiungere tab per la raccolta foto
 
-from turtle import window_width
-
 import PySimpleGUI as sg
+from csv import DictWriter
 
 from slave_module import *
 
@@ -15,7 +14,7 @@ from FileManager.SaveFile import WindowSaveFile
 
 def make_window(theme):
     sg.theme(theme)
-    menu_def = [['&Application', ['&Create and open file (csv)', '&Save file (tiff)']],
+    menu_def = [['&Application', ['&Create and open file']],
                 ['&Help', ['&About']]]
 
     app_layout = [[
@@ -91,16 +90,6 @@ def make_window(theme):
                                        auto_refresh=True)
                       ]]
 
-    graphing_layout = [[
-        sg.Text("Anything you would use to graph will display here!")
-    ],
-                       [
-                           sg.Graph((200, 200), (0, 0), (200, 200),
-                                    background_color="black",
-                                    key='-GRAPH-',
-                                    enable_events=True)
-                       ], [sg.T('Click anywhere on graph to draw a circle')]]
-
     theme_layout = [[
         sg.Text(
             "See how elements look under different themes by choosing a different theme here!"
@@ -123,7 +112,6 @@ def make_window(theme):
         sg.TabGroup([[
             sg.Tab('Measures', app_layout),
             sg.Tab('Logs', logging_layout),
-            sg.Tab('Graphs', graphing_layout),
             sg.Tab('Themes', theme_layout)
         ]],
                     key='-TAB GROUP-',
@@ -147,24 +135,25 @@ def make_window(theme):
 
 def main():
     window = make_window(sg.theme())
+    ### USE THREADS FOR LONG-LASTING MEASUREMENTS!
+    fpath = None
 
-    ### USE THREADS FOR LONG-LASTING MEASUREMENTS
     while True:  # This is the Event Loop
         event, values = window.read(timeout=100)
         if event not in (sg.TIMEOUT_EVENT, sg.WIN_CLOSED):
-            print('============ Event = ', event, ' ==============')
-            print('-------- Values Dictionary (key=value) --------')
-            for key in values:
-                print(key, ' = ', values[key])
+            print('=======================================\n', 'EVENT = ',
+                  event)
+            for key in values:  # TODO: add spaces depending o the length of the values[key]
+                print(' ' * 6, key, ' = ', values[key])
         if event in (None, 'Exit'):
-            print("[LOG] Clicked Exit!")
+            print("EVENT = Clicked Exit!")
             break
 
         ### SENSOR ACQUISITION ###
         elif event == 'Air temperature':
             meas = TemperatureMeasure()
             window['-AIR DISPLAY-'].update("{:.2f}".format(
-                meas.measure_avg_3()))
+                meas.measure_avg_3()))  # TODO: change here for acquisition
             progress_bar = window['-PROGRESS BAR AIR-']
             [progress_bar.update(current_count=i + 1) for i in range(100)]
             print("[LOG] Temperature measurement complete!")
@@ -172,7 +161,7 @@ def main():
         elif event == 'Canopy temperature':
             meas = TemperatureMeasure()
             window['-CANOPY DISPLAY-'].update("{:.2f}".format(
-                meas.measure_avg_3()))
+                meas.measure_avg_3()))  # TODO: change here for acquisition
             progress_bar = window['-PROGRESS BAR CANOPY-']
             [progress_bar.update(current_count=i + 1) for i in range(100)]
             print("[LOG] Temperature measurement complete!")
@@ -180,7 +169,7 @@ def main():
         elif event == 'Dew temperature':
             meas = TemperatureMeasure()
             window['-DEW DISPLAY-'].update("{:.2f}".format(
-                meas.measure_avg_3()))
+                meas.measure_avg_3()))  # TODO: change here for acquisition
             progress_bar = window['-PROGRESS BAR DEW-']
             [progress_bar.update(current_count=i + 1) for i in range(100)]
             print("[LOG] Temperature measurement complete!")
@@ -188,7 +177,7 @@ def main():
         elif event == 'Wind speed':
             meas = TemperatureMeasure()
             window['-WIND DISPLAY-'].update("{:.2f}".format(
-                meas.measure_avg_3()))
+                meas.measure_avg_3()))  # TODO: change here for acquisition
             progress_bar = window['-PROGRESS BAR WIND-']
             [progress_bar.update(current_count=i + 1) for i in range(100)]
             print("[LOG] Temperature measurement complete!")
@@ -196,7 +185,7 @@ def main():
         elif event == 'Pressure':
             meas = TemperatureMeasure()
             window['-PRESSURE DISPLAY-'].update("{:.2f}".format(
-                meas.measure_avg_3()))
+                meas.measure_avg_3()))  # TODO: change here for acquisition
             progress_bar = window['-PROGRESS BAR PRESSURE-']
             [progress_bar.update(current_count=i + 1) for i in range(100)]
             print("[LOG] Temperature measurement complete!")
@@ -204,17 +193,10 @@ def main():
         elif event == 'Solar radiation':
             meas = TemperatureMeasure()
             window['-RADIATION DISPLAY-'].update("{:.2f}".format(
-                meas.measure_avg_3()))
+                meas.measure_avg_3()))  # TODO: change here for acquisition
             progress_bar = window['-PROGRESS BAR RADIATION-']
             [progress_bar.update(current_count=i + 1) for i in range(100)]
             print("[LOG] Temperature measurement complete!")
-
-        # elif event == "-GRAPH-":
-        #     graph = window['-GRAPH-']  # type: sg.Graph
-        #     graph.draw_circle(values['-GRAPH-'],
-        #                       fill_color='yellow',
-        #                       radius=20)
-        #     print("[LOG] Circle drawn at: " + str(values['-GRAPH-']))
 
         elif event == "Set Theme":
             print("[LOG] Clicked Set Theme!")
@@ -227,11 +209,16 @@ def main():
         elif event == 'Create and open file':
             print('[LOG] Clicked Create and open file')
             window_open_file = WindowOpenFile()
-            fp = window_open_file.getfpointer()
+            fpath = window_open_file.getfilename()[:]  # attach [:] to make a copy of the string
+            del window_open_file
 
-        elif event == 'Save file':
-            print('[LOG] Clicked Save file')
+        ### SAVE DATA TO FILE ###
+        # TODO: check here: https://www.delftstack.com/howto/python/python-append-to-csv/
+        elif event == 'Save':
+            print('[LOG] Clicked Save!')
+            f = open(fpath, 'a')
             
+            f.close()
 
         elif event == 'About':
             print("[LOG] Clicked About!")
@@ -249,10 +236,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # sg.theme('black')
-    # sg.theme('dark red')
-    sg.theme('dark green 7')
-    sg.theme('BluePurple')
     sg.theme('Python')
-    # sg.theme('DefaultNoMoreNagging')
     main()
