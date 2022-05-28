54a191ab631c56fd7140b587f213edd7261d3216
Ducci <gabriele.balducci@mail.polimi.it>
2022-05-28 14:53:45 +0200

    Modulo per la camera termica completato (da notare che si prova a fare una media su due secondi, cambiare eventualmente questa impostazione)

--- Sensors/Camera.py
+++ Sensors/Camera.py
@@ -1,39 +1,33 @@
-# TO BE FINISHED
+# pip install PyMLX90614
 
-# sudo pip3 install matplotlib scipy numpy
-# sudo apt-get install -y python-smbus
-# sudo apt-get install -y i2c-tools
-# sudo reboot
-# The address for I2C of Camera should be 0x33
+# from smbus2 import SMBus
+import smbus
+import time
+from mlx90614 import MLX90614
 
-# sudo pip3 install RPI.GPIO adafruit-blinka
-# sudo pip3 install adafruit-circuitpython-mlx90640
+class MLX90614_GY906():
 
-import board, busio
-import numpy as np
-import adafruit_mlx90640
-from scipy import ndimage
+    def __init__(self, name='Camera Ad Infrarosso Per la Temperatura Superficiale'):
+        self.name = name
+        # Your InfraRed sensor will be an instance of DHT22
+        bus = smbus.SMBus(1)
+        self.sensor = MLX90614(bus, address=0x5A)
+    
+    def TemperatureMeasure(self):
+        return self.sensor.get_amb_temp()
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
-# begin MLX90640 with I2C comm
-mlx = adafruit_mlx90640.MLX90640(i2c)
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ  # set refresh rate
-# mlx90640 shape
-mlx_shape = (24, 32)
+    def measure(self):
+        tempSuperficiale = 0
+        n_meas = 10
+        for i in range(n_meas):
+            tempSuperficiale = tempSuperficiale + self.sensor.get_obj_temp()/n_meas
+            time.sleep(0.2)
+        return tempSuperficiale
 
-# # IF u want to interpolate
-# mlx_interp_val = 10 # interpolate # on each dimension
-# mlx_interp_shape = (mlx_shape[0]*mlx_interp_val,
-#                     mlx_shape[1]*mlx_interp_val) # new shape
+def main():
+    w = MLX90614_GY906('Sensore di Temperatura Superficiale')
+    fp = w.measure()
+    print(fp, type(fp))
 
-frame = np.zeros(mlx_shape[0] * mlx_shape[1])  # 768 pts
-while True:
-    try:
-        mlx.getFrame(frame)  # read MLX temperatures into frame var
-        break
-    except ValueError:
-        continue  # if error, just read again
-
-# print out the average temperature from the MLX90640
-print('Average MLX90640 Temperature: {0:2.1f}C'.\
-      format(np.mean(frame)))
+if __name__ == '__main__':
+    main()
